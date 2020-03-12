da26218bdfa41398d9c57fceca276981a0fee7ac
Keshini Jayasinghe <keshinijayasinghe@Keshinis-MacBook-Pro.local>
2020-03-12 14:42:40 +0530

    vals

--- src/main/scala/com/rklick/graphx/process/GraphXMainProcess.scala
+++ src/main/scala/com/rklick/graphx/process/GraphXMainProcess.scala
@@ -19,15 +19,18 @@ object GraphXMainProcess extends GraphXProcess {
     * @return
     */
   def main(args: Array[String]) {
-    val SCHEMA_OPTIONS = Map("header" -> "true", "inferSchema" -> "true")
-    val path = "src/main/resources/graph_data.csv"
-    val df = sqlContext.read.format("csv").options(SCHEMA_OPTIONS).load(path)
-    val graphComponent = GraphComponent("ID", "User", "Relationship", "RelationId")
-    processGraph(df, graphComponent) match {
-      case Right(data) => println(s"GraphX process successfully completed.")
-        sqlContext.sparkContext.stop()
-      case Left(error) => println(s"Error during graphX:::${error}")
-        sqlContext.sparkContext.stop()
+    while(true){
+      val SCHEMA_OPTIONS = Map("header" -> "true", "inferSchema" -> "true")
+      val path = "src/main/resources/graph_data.csv"
+      val df = sqlContext.read.format("csv").options(SCHEMA_OPTIONS).load(path)
+      val graphComponent = GraphComponent("ID", "User", "Relationship", "RelationId")
+      processGraph(df, graphComponent) match {
+        case Right(data) => println(s"GraphX process successfully completed.")
+          sqlContext.sparkContext.stop()
+        case Left(error) => println(s"Error during graphX:::${error}")
+          sqlContext.sparkContext.stop()
+      }
     }
+    
   }
 }
